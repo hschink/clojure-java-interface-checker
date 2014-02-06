6a5f43fe9d44767dabca7382a7b90179e4ca76ca
Hagen Schink <hagen.schink@gmail.com>
2014-02-06 20:10:50 +0100

    Adds copyright header

--- src/main/clojure/org/iti/clojureJavaInterfaceVerifier/main.clj
+++ src/main/clojure/org/iti/clojureJavaInterfaceVerifier/main.clj
@@ -1,4 +1,20 @@
-; java -cp "../clojure-1.5.1.jar;../jgrapht-0.8.3.jar;./dependencies/structureGraph-0.0.1.jar;./build/libs/clojure-java-interface-verifier-0.0.1.jar" "org.iti.clojureJavaInterfaceVerifier.Main" -f src
+;;
+;;  Copyright 2014 Hagen Schink <hagen.schink@gmail.com>
+;;
+;;  This file is part of sql-schema-comparer.
+;;
+;;  sql-schema-comparer is free software: you can redistribute it and/or modify
+;;  it under the terms of the GNU Lesser General Public License as published by
+;;  the Free Software Foundation, either version 3 of the License, or
+;;  (at your option) any later version.
+;;
+;;  sql-schema-comparer is distributed in the hope that it will be useful,
+;;  but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;  GNU Lesser General Public License for more details.
+;;
+;;   You should have received a copy of the GNU Lesser General Public License
+;;  along with sql-schema-comparer.  If not, see <http://www.gnu.org/licenses/>.
 
 (ns org.iti.clojureJavaInterfaceVerifier.Main
   (:require [clojure.string :as string]
