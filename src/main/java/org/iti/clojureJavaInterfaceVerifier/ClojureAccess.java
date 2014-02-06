ffa0bb5dec786ac45ea09ed520a3f10eef27279a
Hagen Schink <hagen.schink@gmail.com>
2014-02-06 20:10:01 +0100

    Moves java test file

---
+++ src/main/java/org/iti/clojureJavaInterfaceVerifier/ClojureAccess.java
@@ -0,0 +1,39 @@
+/*
+ *  Copyright 2014 Hagen Schink <hagen.schink@gmail.com>
+ *
+ *  This file is part of sql-schema-comparer.
+ *
+ *  sql-schema-comparer is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Lesser General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  sql-schema-comparer is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with sql-schema-comparer.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ *
+ */
+
+package org.iti.clojureJavaInterfaceVerifier;
+
+import java.io.IOException;
+
+public class ClojureAccess {
+
+	/**
+	 * @param args
+	 * @throws IOException 
+	 */
+	public static void main(String[] args) throws IOException {
+		System.out.println("Working Directory = " + System.getProperty("user.dir"));
+//		RT.loadResourceScript("src/main/clojure/org/iti/clojureJavaInterfaceVerifier/test.clj");
+		new clojure.lang.RT();
+		clojure.lang.Compiler.loadFile("src/main/clojure/org/iti/clojureJavaInterfaceVerifier/test.clj");
+	}
+
+}
