7434e08c506a1d428dcc41061e9a0eb141335c9a
Hagen Schink <hagen.schink@gmail.com>
2014-02-15 17:49:37 +0100

    Fixes copyright header

--- src/test/clojure/files/ClojureAccess.java
+++ src/test/clojure/files/ClojureAccess.java
@@ -1,20 +1,20 @@
 /*
  *  Copyright 2014 Hagen Schink <hagen.schink@gmail.com>
  *
- *  This file is part of sql-schema-comparer.
+ *  This file is part of clojure-java-interface-verifier.
  *
- *  sql-schema-comparer is free software: you can redistribute it and/or modify
+ *  clojure-java-interface-verifier is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU Lesser General Public License as published by
  *  the Free Software Foundation, either version 3 of the License, or
  *  (at your option) any later version.
  *
- *  sql-schema-comparer is distributed in the hope that it will be useful,
+ *  clojure-java-interface-verifier is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
  *  You should have received a copy of the GNU Lesser General Public License
- *  along with sql-schema-comparer.  If not, see <http://www.gnu.org/licenses/>.
+ *  along with clojure-java-interface-verifier.  If not, see <http://www.gnu.org/licenses/>.
  *
  *
  */
