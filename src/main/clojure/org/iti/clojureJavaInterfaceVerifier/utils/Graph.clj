59cab847b36a00affed0660e22de8ccd9b1ba5b3
Hagen Schink <hagen.schink@gmail.com>
2015-08-15 12:46:21 +0200

    Integrate statement comparer

--- src/main/clojure/org/iti/clojureJavaInterfaceVerifier/utils/Graph.clj
+++ src/main/clojure/org/iti/clojureJavaInterfaceVerifier/utils/Graph.clj
@@ -22,7 +22,7 @@
         [clojure.reflect :only [reflect]]
         [org.iti.clojureJavaInterfaceVerifier.Nodes :only [element]])
   (:import (org.iti.structureGraph StructureGraph)
-           (org.iti.structureGraph.comparison SimpleStructureGraphComparer)
+           (org.iti.structureGraph.comparison StatementStructureGraphComparer)
            (org.iti.structureGraph.comparison.result Type)
            (org.jgrapht.graph SimpleDirectedGraph DefaultEdge)
            (org.iti.clojureJavaInterfaceVerifier.edges HasParameter HasMethod HasNamespace)))
@@ -33,24 +33,24 @@
 
 (defrecord File [name namespaces])
 
-(defn- add-parameter-to-graph [graph source parameter]
-  (let [parameter-element (element parameter)]
+(defn- add-parameter-to-graph [graph source mandatory parameter]
+  (let [parameter-element (element parameter mandatory)]
     (do
       (.addVertex graph parameter-element)
       (.addEdge graph source parameter-element (HasParameter.)))))
 
-(defn- add-function-to-graph [graph source function]
-  (let [element (element (:name function))]
+(defn- add-function-to-graph [graph source mandatory function]
+  (let [element (element (:name function) false)]
     (do
        (.addVertex graph element)
        (.addEdge graph source element (HasMethod.))
        (doall
-         (map (partial add-parameter-to-graph graph element) (:parameters function))))))
+         (map (partial add-parameter-to-graph graph element mandatory) (:parameters function))))))
 
-(defn- add-namespace-to-graph [graph file namespace]
+(defn- add-namespace-to-graph [graph file mandatory namespace]
   (let [ns-name (:name namespace)
         is-default-ns (= :default ns-name)
-        ns-element (if is-default-ns nil (element (:name namespace)))
+        ns-element (if is-default-ns nil (element (:name namespace) false))
         source (if is-default-ns file ns-element)]
     (do
       (if (not is-default-ns)
@@ -58,35 +58,38 @@
           (.addVertex graph ns-element)
           (if-not (nil? file) (.addEdge graph file ns-element (HasNamespace.)))))
       (doall
-        (map (partial add-function-to-graph graph source) (:functions namespace))))))
+        (map (partial add-function-to-graph graph source mandatory) (:functions namespace))))))
 
-(defn- add-file-to-graph [graph file]
-  (let [file-element (element (:name file))]
+(defn- add-file-to-graph [graph file mandatory]
+  (let [file-element (element (:name file) false)]
     (do
       (.addVertex graph file-element)
       (doall
-        (map (partial add-namespace-to-graph graph file-element) (:namespaces file))))))
+        (map (partial add-namespace-to-graph graph file-element mandatory) (:namespaces file))))))
 
-(defn- add-element-to-graph [graph element]
+(defn- add-element-to-graph [graph mandatory element]
   (let [type-of-element (type element)]
     (cond
-      (= type-of-element Namespace) (add-namespace-to-graph graph nil element)
-      (= type-of-element File) (add-file-to-graph graph element))))
+      (= type-of-element Namespace) (add-namespace-to-graph graph nil mandatory element)
+      (= type-of-element File) (add-file-to-graph graph element mandatory))))
 
-(defn- add-elements-to-graph [graph files]
+(defn- add-elements-to-graph [graph files mandatory]
   (doall
-    (map (partial add-element-to-graph graph) files)))
+    (map (partial add-element-to-graph graph mandatory) files)))
 
-(defn create-structure-graph [methods-by-namespace]
+(defn create-structure-graph-with-mandatory-functions [methods-by-namespace mandatory]
   (let [graph (SimpleDirectedGraph. DefaultEdge)]
-    (add-elements-to-graph graph methods-by-namespace)
+    (add-elements-to-graph graph methods-by-namespace mandatory)
     (StructureGraph. graph)))
 
+(defn create-structure-graph [methods-by-namespace]
+  (create-structure-graph-with-mandatory-functions methods-by-namespace false))
+
 (defn- compare-clojure-java-graphs [clojure-graph java-graph]
-  (let [comparer (SimpleStructureGraphComparer.)
-        clojure-structure-graph (create-structure-graph clojure-graph)
+  (let [comparer (StatementStructureGraphComparer.)
+        clojure-structure-graph (create-structure-graph-with-mandatory-functions clojure-graph true)
         java-structure-graph (create-structure-graph java-graph)
-        result (.compare comparer clojure-structure-graph java-structure-graph)]
+        result (.compare comparer java-structure-graph clojure-structure-graph)]
     result))
 
 (declare normalize-clojure-funcs)
@@ -102,44 +105,13 @@
 (defn- normalize-clojure-funcs [clojure-functions]
   (map normalize-clojure-func clojure-functions))
 
-(defn- is-referenced-ns? [ns-list ns]
-  (some #(.startsWith ns %) ns-list))
-
-(defn- number-of-dots [path]
-  (count (filter #(= (str %) ".") path)))
-
-(defn- shortest-path-by-ns [pathes ns]
-  (let [pathes-by-ns (filter #(.startsWith % ns) pathes)]
-    (if (empty? pathes-by-ns)
-      '()
-      (let [shortes-dot-count (apply min (map number-of-dots pathes-by-ns))
-            shortest-pathes-by-ns (filter #(= (number-of-dots %) shortes-dot-count) pathes-by-ns)]
-        shortest-pathes-by-ns))))
-
-(defn- path-in-list? [list path]
-  (some #(= path %) list))
-
-(defn- remove-subsequent-mods [referenced-ns modifications]
-  (let [pathes (keys modifications)
-        shortest-pathes (mapcat (partial shortest-path-by-ns pathes) referenced-ns)
-        removed-subsequent-mods (into {} (filter #(path-in-list? shortest-pathes (key %)) modifications))]
-    removed-subsequent-mods))
-
-(defn- remove-none-parameter-node-additions [modifications]
-  (let [check (fn [mod] (or (not (empty? (re-seq #"HasParameter" (key mod)))) (not= (.getType (val mod)) Type/NodeDeleted)))]
-  (into {} (filter check modifications))))
-
-(defn- filter-modifications [referenced-ns modifications]
-  (let [removed-unreferenced-ns (into {} (filter #(is-referenced-ns? referenced-ns (key %)) modifications))
-        removed-subsequent-mods (remove-subsequent-mods referenced-ns removed-unreferenced-ns)
-        removed-none-parameter-node-additions (remove-none-parameter-node-additions removed-subsequent-mods)]
-    removed-none-parameter-node-additions))
-
 (defn check-clojure2java-function-mapping [clojure-functions java2clojure-calls]
-  (let [referenced-ns (map :name java2clojure-calls)
-        clojure-funcs-without-files (mapcat :namespaces clojure-functions)
+  (let [clojure-funcs-without-files (mapcat :namespaces clojure-functions)
         normalized-clojure-funcs (normalize-clojure-funcs clojure-funcs-without-files)
         result (compare-clojure-java-graphs normalized-clojure-funcs java2clojure-calls)
-        modifications (.getModifications result)
-        filtered-mods (filter-modifications referenced-ns modifications)]
-    filtered-mods))
+        check (fn [mod]
+                (let [type (.getType (val mod))]
+                  (or (= type Type/NodeAdded)
+                      (= type Type/NodeDeleted))))
+        modifications (into {} (filter check (.getModifications result)))]
+     modifications))
